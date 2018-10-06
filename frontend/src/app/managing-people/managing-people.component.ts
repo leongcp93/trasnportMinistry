e15f4f4ec13d883dde471dffd23553e4dd75e9b8
Kenneth_Guo <kwokkinhungisme@gmail.com>
2018-10-06 21:15:04 +1000

    removed one line of code

--- frontend/src/app/managing-people/managing-people.component.ts
+++ frontend/src/app/managing-people/managing-people.component.ts
@@ -76,9 +76,9 @@ export class ManagingPeopleComponent implements OnInit {
     this.drivers = this.ms.drivers;
     this.passengers = this.ms.passengers;
     this.totalSeats = this.ms.totalSeats;
-    this.route.queryParams.subscribe(params => {
+    /*(this.route.queryParams.subscribe(params => {
       let token = params['token'];
-  });
+  });*/
   }
 
   popUp() {
